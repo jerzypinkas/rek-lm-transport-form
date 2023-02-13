b8c7a4e8c98003acb25d5de43c0a2c4cce272c1b
root <root@PC382.bsbox.pl>
2023-02-13 12:28:06 +0100

    add tmp post action

--- sites/api/src/Controller/AirplaneController.php
+++ sites/api/src/Controller/AirplaneController.php
@@ -25,4 +25,35 @@ class AirplaneController extends FrontendController
 
         return $this->json(["success" => true, "data" => $data]);
     }
+
+    #[Route('/api/airplanes', name: "post_airplane", methods: ['POST'])]
+    public function postAction(): Response
+    {
+
+        $airplane1 = new DataObject\Airplane();
+
+//        $airplane1->setKey('Airbus A380');
+        $airplane1->setKey(\Pimcore\Model\Element\Service::getValidKey('Airbus A380', 'object'));
+        $airplane1->setParentId(1);
+
+        $airplane1->setName('Airbus A380');
+        $airplane1->setPayload(35000);
+        $airplane1->setPublished(true);
+
+        $airplane1->save();
+
+        $airplane2 = new DataObject\Airplane();
+
+//        $airplane2->setKey('Boeing 747');
+        $airplane2->setKey(\Pimcore\Model\Element\Service::getValidKey('Boeing 747', 'object'));
+        $airplane2->setParentId(1);
+
+        $airplane2->setName('Boeing 747');
+        $airplane2->setPayload(38000);
+        $airplane2->setPublished(true);
+
+        $airplane2->save();
+
+        return $this->json(["success" => true, "data" => '']);
+    }
 }
